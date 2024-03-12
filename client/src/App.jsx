fa052a7d9a5ec3dd588e103c203ae9e17d597932
andrew <andrewlin1368@gmail.com>
2024-03-12 13:58:46 -0400

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { store } from "./api/store";
 import Home from "./components/Home";
+import Account from "./components/Account";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/account" element={<Account></Account>}></Route>
         </Routes>
       </BrowserRouter>
     </Provider>
